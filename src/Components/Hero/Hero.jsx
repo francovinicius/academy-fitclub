7c7fb8cace9015f3a3daded10e057b69cdee34fd
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-05-31 00:02:25 -0300

    alt span

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,13 +1,50 @@
 import Header from '../Header/Header'
 import styles from './Hero.css'
 
-export default function Hero () {
-   return (
+export default function Hero() {
+    return (
 
         <div className="hero">
-            <div className="left-h"><Header /></div>
+            <div className="left-h">
+                <Header />
+                <div className="the-best-ad">
+                    <div></div>
+                    <span>the best fitness club in the town</span>
+                </div>
+
+                <div className="hero-text">
+                    <div>
+                        <span className='stroke-text'>Shape </span>
+                        <span>Your</span>
+                    </div>
+                    <div><span>Ideal Body</span></div>
+
+                    <div>
+                        <span>
+                            In here we will help you to shape and build your ideal body and live up your life to fullest
+                        </span>
+                    </div>
+
+                    <div className="figures">
+                        <div>
+                            <span></span>
+                            <span></span>
+                        </div>
+                        <div>
+                            <span></span>
+                            <span></span>
+                        </div>
+                        <div>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
             <div className="right-h">right side</div>
         </div>
 
-   ) 
+    )
 }
