39e515ce28231fc2168e769d2bc65bc621a2583a
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-06-01 14:31:29 -0300

    add section reasons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Hero from './Components/Hero/Hero';
 import Programs from './Components/Programs/Programs';
+import Reasons from './Components/Reassons/Reasons';
 function App() {
   return (
     <div className="App">
           <Hero />
           <Programs />
+          <Reasons /> 
     </div>
   );
 }
