a92484fa8b2cf042e5a17881e14699d28797aa91
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-06-02 10:54:52 -0300

    finalized the Testmonials section

--- src/assets/Testmonials/Testimonials.jsx
+++ src/assets/Testmonials/Testimonials.jsx
@@ -1,6 +1,8 @@
 import styles from './Testimonials.css'
 import { testimonialsData } from '../../data/testimonialsData'
 import { useState } from 'react'
+import leftArrow from '../../assets/leftArrow.png'
+import rightArrow from '../../assets/rightArrow.png'
 
 export default function Testimonials() {
     const [selected, setSelected] = useState(0);
@@ -21,7 +23,29 @@ export default function Testimonials() {
                     - {testimonialsData[selected].status}
                 </span>
             </div>
-            <div className="right-t"></div>
+            <div className="right-t">
+                <div></div>
+                <div></div>
+                <img src={testimonialsData[selected].image} alt="" srcset="" />
+                <div className="arrows">
+
+                    <img
+                    onClick={()=>{
+                        selected=== 0
+                        ? setSelected(tLenght - 1)
+                        : setSelected((prev) => prev - 1);
+                    }}
+                    src={leftArrow} alt=""/>
+
+                    <img
+                    onClick={()=>{
+                        selected=== tLenght - 1
+                        ? setSelected(0)
+                        : setSelected((prev) => prev + 1);
+                    }}
+                     src={rightArrow} alt=""/>
+                </div>
+            </div>
         </div>
     )
 }
